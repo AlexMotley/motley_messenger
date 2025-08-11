88b59a57ecbfcafa0b12741b732460676bab013b
Aleksandr Balandin <a.balandin@napoleonit.ru>
2025-08-11 13:05:09 +0500

    feat: Добавил enum в модель

--- app/models/dialogue_participant.py
+++ app/models/dialogue_participant.py
@@ -2,20 +2,21 @@ from app.db.base_class import BaseModel
 from sqlalchemy import Integer, String, DateTime, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 import uuid
+import enum
 from datetime import datetime
 
 
 class DialogueParticipant(BaseModel):
-    class Roles:
-        ADMIN: str = 'admin'
-        MEMBER: str = 'member'
+    class RolesEnum(str, enum.Enum):
+        ADMIN = 'admin'
+        MEMBER = 'member'
 
     __tablename__ = 'dialogue_participants'
     
     id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
     user_id: Mapped[uuid.UUID] = mapped_column(ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
     dialogue_id: Mapped[int] = mapped_column(Integer, ForeignKey('dialogues.id', ondelete='CASCADE'), nullable=False)
-    role: Mapped[str] = mapped_column(String, default=Roles.MEMBER, nullable=False)
+    role: Mapped[RolesEnum] = mapped_column(String, default=RolesEnum.MEMBER, nullable=False)
     joined_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.utcnow, nullable=False)
 
     dialogue: Mapped['Dialogue'] = relationship(back_populates='participants')
