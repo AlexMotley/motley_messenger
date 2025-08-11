6222908cf687a283ded4808e099911cfc671a42c
Aleksandr Balandin <a.balandin@napoleonit.ru>
2025-08-11 13:16:18 +0500

    fix: Починил подключение к БД

--- app/db/session.py
+++ app/db/session.py
@@ -1,7 +1,11 @@
 from app.core.config import get_db_url
-from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
 
 
 DATABASE_URL = get_db_url()
 engine = create_async_engine(url=DATABASE_URL, future=True)
 SessionLocal = async_sessionmaker(bind=engine, autoflush=False, autocommit=False)
+
+async def get_db() -> AsyncSession:
+    async with SessionLocal() as session:
+        yield session
